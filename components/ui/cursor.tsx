7dd532bf2cdb839810f4c485dd6fdb8b1a0cba47
Adithya Rao K <116443929+Adithya11811@users.noreply.github.com>
2024-03-27 08:17:46 +0530

    feat: custom cursor

---
+++ components/ui/cursor.tsx
@@ -0,0 +1,46 @@
+'use client'
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import Link from 'next/link';
+
+export default function BlurryCursor(): JSX.Element {
+    const mouse = useRef<{ x: number; y: number }>({ x: 50, y: 50 });
+    const button = useRef<HTMLButtonElement>(null);
+
+    const manageMouseMove = (e: MouseEvent): void => {
+        const { clientX, clientY } = e;
+
+        mouse.current = {
+            x: clientX,
+            y: clientY
+        }
+
+        moveButton(mouse.current.x, mouse.current.y);
+    }
+
+    const moveButton = (x: number, y: number): void => {
+        gsap.set(button.current, { x, y, xPercent: -50, yPercent: -50 })
+    }
+
+    useEffect(() => {
+        window.addEventListener("mousemove", manageMouseMove);
+        return () => {
+            window.removeEventListener("mousemove", manageMouseMove);
+        }
+    }, []);
+
+    return (
+      <div className="absolute z-50 h-screen">
+        <Link href="/auth/register">
+          <button
+            ref={button}
+            className="top-0 left-0 fixed bg-green-500 text-white rounded-full px-4 py-6 z-10"
+          >
+            Get
+            <br />
+            Started
+          </button>
+        </Link>
+      </div>
+    )
+}
